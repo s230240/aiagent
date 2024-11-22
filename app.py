292f344d9ae106d839b030a87194225ee0ecfeda
s230240 <gimpo.s230240@ggh.goe.go.kr>
2024-11-22 14:28:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 # 분류 모델 웹앱 만들기
-
 import streamlit as st
 
 # 1. 기계학습 모델 파일 로드
 
 
 # 2. 모델 설명
- 
+st.title('지능형 환율 예측 에이전트') 
 
 # 3. 데이터시각화
 
 
 # 4. 모델 활용
+st.subheader('모델 활용')
+st.write('환율을 예측하고 싶으시다면, 1을 눌러주세요.')
+a = st.selectbox('환율예측 확인 입력(확인한다:0, 확인하지않는다:1',[0,1])
+
+if st.button('환율 예측'):
+  input_data = [[ a ]]
+ p = model.predict(input_data)
+ st.write('환율예측 결과는',p)
