5e29754d86d083afec197035be3b4fbc99a70391
s230240 <gimpo.s230240@ggh.goe.go.kr>
2024-11-26 16:12:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,22 @@ model = joblib.load('')
 
 # 2. 모델 설명
 st.title('지능형 환율 예측 에이전트') 
-
+st.subheader('모델 설명')
+st.write('- 기계학습 알고리즘 : 선형 회귀')
+st.write('- 학습 데이터 출처 : https://www.index.go.kr/enara')
+st.write('- 훈련 데이터 개수: 92건')
+st.write('- 테스트 데이터 개수: 92건')
+st.write('- 문제: 환율 예측 기술이 잘못된 목적으로 사용되어 시장을 조작하거나 부당한 이익을 취하려는 시도가 발생할 수 있음')
+st.write('- 대응방안: 기술 사용 목적과 허용범위를 법적으로 규제 필요')
+         
 # 3. 데이터시각화
 col1,col2 = st.columns(2)
 with col1:
-  st.subheader('데이터시각화1')
-  st.image
+st.subheader('데이터시각화1')
+st.image
 with col2:('image(1).png')
-  st.subheader('데이터시각화2')
-  st.image('image.png')  
+st.subheader('데이터시각화2')
+st.image('image.png')  
 
 # 4. 모델 활용
 st.subheader('모델 활용')
